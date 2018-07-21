ab6f940d5c332ecb402c6408eaee3bdd06f27893
Maik Blumenthal <m@element36.io>
2018-07-21 16:02:29 +0200

    fix: env variable

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,5 +1,8 @@
+console.log(process.env.NODE_ENV);
+console.log(process.env.REACT_ENV);
+
 let url = 'http://localhost:8080';
-if (process.env.NODE_ENV === 'staging') url = 'https://cash36-backend.herokuapp.com';
-if (process.env.NODE_ENV === 'production') url = 'https://cash36-test-backend.herokuapp.com';
+if (process.env.NODE_ENV === 'staging'||process.env.REACT_ENV === 'staging') url = 'https://cash36-backend.herokuapp.com';
+if (process.env.NODE_ENV === 'production'||process.env.REACT_ENV === 'production') url = 'https://cash36-test-backend.herokuapp.com';
 
 export const API_ROOT = url;
