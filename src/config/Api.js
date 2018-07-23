e1def7a00e534ebf37dbf4ec33d2a4056e325920
Maik Blumenthal <m@element36.io>
2018-07-23 09:51:58 +0200

    fix: env variable

--- src/config/Api.js
+++ src/config/Api.js
@@ -3,10 +3,10 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 // Load the env object.
 const env = runtimeEnv();
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.REACT_ENV);
-console.log(env.NODE_ENV);
-console.log(env.REACT_ENV);
+console.log(process.env.REACT_APP_NODE_ENV);
+console.log(process.env.REACT_APP_REACT_ENV);
+console.log(env.REACT_APP_NODE_ENV);
+console.log(env.REACT_APP_REACT_ENV);
 
 let url = 'http://localhost:8080';
 if (process.env.NODE_ENV === 'staging' || env.REACT_ENV === 'staging') url = 'https://cash36-backend.herokuapp.com';
