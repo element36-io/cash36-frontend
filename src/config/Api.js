96ef3379383bd201db14909510299cdbec3a279f
Maik Blumenthal <m@element36.io>
2018-10-04 20:57:00 +0200

    add env dependent url for test system

--- src/config/Api.js
+++ src/config/Api.js
@@ -12,8 +12,8 @@ if (apiEnv === 'staging') {
     web3NodeUrl = 'http://167.99.243.81:8866/';
 }
 if (apiEnv === 'production') {
-    url = 'https://cash36-test-backend.herokuapp.com';
-    web3NodeUrl = 'https://rinkeby.infura.io/v3/4cf538c6b0bb4562bdc2f31430327c08';
+    url = 'https://167.99.243.81:9090';
+    web3NodeUrl = 'http://167.99.243.81:8866/';
 }
 
 export const API_ROOT = url;
