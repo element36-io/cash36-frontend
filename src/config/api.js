03f6ef2c1a8cfb08ecc11a008bd67537036b4c8c
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2018-10-10 13:28:17 +0000

    feature/user-login

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv();
@@ -18,3 +19,61 @@ if (apiEnv === 'production') {
 
 export const API_ROOT = url;
 export const WEB3_NODE = web3NodeUrl;
+
+const api = axios.create({
+  baseURL: API_ROOT
+});
+
+const getRefreshedToken = async (refreshToken) => {
+  const config = {
+    data: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+    headers: {
+      'Authorization': 'Basic Y2FzaDM2LWNsaWVudDpjYXNoMzYtc2VjcmV0',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+  // Tries to get a refresh token, if it fails, sends 401.
+  try {
+    const response = await axios.post(`${API_ROOT}/oauth/token`, config.data, {
+      headers: config.headers
+    });
+
+    const token = response.data;
+
+    sessionStorage.setItem('access_token', token.access_token);
+    sessionStorage.setItem('refresh_token', token.refresh_token);
+    sessionStorage.setItem('expires_at', new Date().getTime() + (token.expires_in * 1000));
+
+    return token.access_token;
+  } catch (error) {
+    console.log('Token expired, logging you out...');
+  }
+};
+
+// Intercept the request and inject the token into headers. Check if token is expired and call getRefreshedToken function if needed.
+api.interceptors.request.use(
+  async function (config) {
+    const getToken = async () => {
+      const token = sessionStorage.getItem('access_token');
+      const refreshToken = sessionStorage.getItem('refresh_token');
+      const expiresAt = sessionStorage.getItem('expires_at');
+
+      // Check if expired and if true, get new
+      if (token && new Date().getTime() > expiresAt) {
+        return getRefreshedToken(refreshToken);
+      } else {
+        return token;
+      }
+    };
+    // set access_token
+    const token = await getToken();
+    // add access_token to the request headers
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
