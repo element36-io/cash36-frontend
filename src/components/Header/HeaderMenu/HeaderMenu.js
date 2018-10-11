2e7519c0fea63d26d32c7c630cdf475c0f42fbb4
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-10-11 13:31:38 +0200

    Add Quick Actions with Buttons, as Links

--- src/components/Header/HeaderMenu/HeaderMenu.js
+++ src/components/Header/HeaderMenu/HeaderMenu.js
@@ -7,41 +7,41 @@ import './HeaderMenu.scss';
 
 class HeaderMenu extends Component {
     state = {
-        open: false,
-        anchorEl: null
+      open: false,
+      anchorEl: null
     };
 
     handleClick = evt => {
-        this.setState({ open: true, anchorEl: evt.currentTarget });
+      this.setState({ open: true, anchorEl: evt.currentTarget });
     };
 
     handleClose = () => {
-        this.setState({ open: false, anchorEl: null });
+      this.setState({ open: false, anchorEl: null });
     };
 
     render () {
-        const { open, anchorEl } = this.state;
-        const { logout } = this.props;
+      const { open, anchorEl } = this.state;
+      const { logout } = this.props;
 
-        return (
-            <div className='header__menu'>
-                <span><i className='fas fa-user' /></span>
-                <MenuIcon
-                    onClick={this.handleClick}
-                    className='header__menu__icon'
+      return (
+        <div className='header__menu'>
+          <span><i className='fas fa-user' /></span>
+          <MenuIcon
+            onClick={this.handleClick}
+            className='header__menu__icon'
 
-                />
-                <PopupMenu handleClose={this.handleClose} open={open} anchor={anchorEl} placement="bottom-end">
-                    <MenuItem onClick={logout}>Logout</MenuItem>
-                </PopupMenu>
-            </div>
+          />
+          <PopupMenu handleClose={this.handleClose} open={open} anchor={anchorEl} placement='bottom-end'>
+            <MenuItem onClick={logout}>Logout</MenuItem>
+          </PopupMenu>
+        </div>
 
-        );
+      );
     }
 }
 
 HeaderMenu.propTypes = {
-    logout: PropTypes.func.isRequired
+  logout: PropTypes.func.isRequired
 };
 
 export default HeaderMenu;
---
+++ src/components/QuickActions/buttons.js
@@ -0,0 +1,21 @@
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+export default [
+  {
+    link: '/buy',
+    Icon: AddIcon,
+    label: 'Buy'
+  },
+  {
+    link: '/sell',
+    Icon: RemoveIcon,
+    label: 'Sell'
+  },
+  {
+    link: '/transfer',
+    Icon: ArrowForwardIcon,
+    label: 'Transfer'
+  }
+];
