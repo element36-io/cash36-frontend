e77f2ea4c7c258ddd8b636eff8f3be46b2446382
Vladimir Nikolic <>
2018-10-24 09:15:59 +0200

    Bottom spacing: adding page wrapper class

--- src/views/Buy/Buy.js
+++ src/views/Buy/Buy.js
@@ -17,7 +17,7 @@ class Buy extends Component {
     amount: '',
     symbol: 'EUR36',
     manualTransferData: null
-  }
+  };
 
   nextStep = () => {
     if (this.state.step === 0) {
@@ -25,13 +25,13 @@ class Buy extends Component {
         this.setState({ step: 1 });
       }
     }
-  }
+  };
 
   previousStep = () => {
     this.setState((prevState) => {
       return { step: Math.round(prevState.step - 1) };
     });
-  }
+  };
 
   handleManualTransferClick = async () => {
     const data = {
@@ -48,69 +48,71 @@ class Buy extends Component {
     } catch (error) {
       console.log(error); // TODO, handle the error
     }
-  }
+  };
 
   handleAutoTransferClick = () => {
     console.log('auto transfer chosen');
     this.setState({ step: 2.2 });
-  }
+  };
 
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
-  }
+  };
 
   handleOrderSubmit = () => {
     console.log('order submitted');
-  }
+  };
 
   render () {
     const { step, manualTransferData } = this.state;
     return (
-      <div className='buy paper'>
-        {step > 0 && <BackButton onClick={this.previousStep} />}
-        <Responsive>
-          <Stepper step={step} />
-        </Responsive>
-        <Responsive isMobile>
-          {step !== 2.2 && <Stepper step={step} />}
-        </Responsive>
-        <div className='buy__content'>
-          {step === 0 &&
+      <div className="page-wrapper">
+        <div className='buy paper'>
+          {step > 0 && <BackButton onClick={this.previousStep}/>}
+          <Responsive>
+            <Stepper step={step}/>
+          </Responsive>
+          <Responsive isMobile>
+            {step !== 2.2 && <Stepper step={step}/>}
+          </Responsive>
+          <div className='buy__content'>
+            {step === 0 &&
             <BuyTokens
               handleChange={this.handleChange}
               amount={this.state.amount}
               symbol={this.state.symbol}
               nextStep={this.nextStep}
             />}
-          {step === 1 &&
+            {step === 1 &&
             <PaymentMethod
               next={this.nextStep}
               handleManualTransferClick={this.handleManualTransferClick}
               handleAutoTransferClick={this.handleAutoTransferClick}
             />}
-          {step === 2.1 &&
+            {step === 2.1 &&
             <InitiateManualPayment
               next={this.nextStep}
               handleOrderSubmit={this.handleOrderSubmit}
               transferData={manualTransferData}
             />}
-          {step === 2.2 &&
+            {step === 2.2 &&
             <InitiateAutoPayment
               next={this.nextStep}
             />}
-        </div>
-        <div className='buy__footer'>
-          {(step < 2) &&
+          </div>
+          <div className='buy__footer'>
+            {(step < 2) &&
             <span>
               Buying cash36 Tokens is as simple as a bank transfer. First, choose amount and type of Token you wish to buy.
-              <br />
+              <br/>
               After that you will receive the transfer instructions. Once we receive the amount, the tokens will be credited to your account.
             </span>}
-          {step > 2 &&
+            {step > 2 &&
             <span>
               Please make sure your payment will be triggered from your registered bank account: IBAN CH3343 23 3 32 32 32
             </span>}
+          </div>
         </div>
       </div>
     );
