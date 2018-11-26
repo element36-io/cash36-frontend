6616bfc7e4bd6b7e7ec3d308f618f8bcccef29fc
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-11-26 08:09:24 +0100

    remove text helpers, adjust manual payment.

--- src/helpers/text.helpers.js
+++
@@ -1,7 +0,0 @@
-import { countries } from './text.data';
-
-export const getCountryCode = (countryName) => {
-  const code = countries.filter((country) => country.name === countryName);
-
-  return code[0].code;
-};
