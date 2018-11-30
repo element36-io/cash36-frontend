4f328e062841aadf618d565e1ddaf3325e5ed70a
Maik Blumenthal <m@element36.io>
2018-11-30 14:59:45 +0100

    package to docker and deploy to droplet

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ sshpass -p $1 ssh -o StrictHostKeyChecking=no deploy@167.99.243.81 <<-'ENDSSH'
    sudo docker stop cash36-frontend
    sudo docker rm cash36-frontend
    sudo docker pull registry.gitlab.com/cash36/cash36-frontend
-   sudo docker run --name cash36-frontend -p 3001:3001 -d registry.gitlab.com/cash36/cash36-frontend
+   sudo docker run --name cash36-frontend -p 3000:3000 -d registry.gitlab.com/cash36/cash36-frontend
 ENDSSH
