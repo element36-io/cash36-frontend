8be8b72f19093fd3e658efe3da44a16a825270ef
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-11-19 12:32:46 +0100

    Set new password

--- src/views/ResetPassword/ResetPassword.js
+++ src/views/ResetPassword/ResetPassword.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import AuthWrapper from '../../components/AuthWrapper';
 import AuthForm from '../../components/AuthWrapper/AuthForm';
@@ -11,13 +11,12 @@ import './ResetPassword.scss';
 
 const ResetPassword = () => {
   const [error, setError] = useState(null);
-  const [emailSent, setEmailSent] = useState(false);
+  const history = useHistory();
 
   const submitCallback = async values => {
     try {
       await resetPassword(values.email);
-
-      setEmailSent(true);
+      history.push(`/set-new-password?for=${values.email}`);
     } catch (error) {
       setError(error);
     }
@@ -25,31 +24,22 @@ const ResetPassword = () => {
 
   return (
     <div className="reset-password">
-      <AuthWrapper message={!emailSent ? 'Reset Password' : 'Check your email'}>
-        {!emailSent ? (
-          <Fragment>
-            <p>Enter the email you registered with</p>
-            <AuthForm
-              submitCallback={submitCallback}
-              validationSchema={validationSchema}
-              initialValues={initialValues}
-              formFields={formFields}
-              buttonLabel="Send me Instructions"
-              errorMsg={error}
-            >
-              <p className="paragraph-link-gray">
-                <Link to="/login">Back to login</Link>
-              </p>
-            </AuthForm>
-          </Fragment>
-        ) : (
-          <div className="reset-password__message">
-            Password reset instructions have been sent to your email address.
+      <AuthWrapper message="Reset Password">
+        <Fragment>
+          <p>Enter the email you registered with</p>
+          <AuthForm
+            submitCallback={submitCallback}
+            validationSchema={validationSchema}
+            initialValues={initialValues}
+            formFields={formFields}
+            buttonLabel="Send me Instructions"
+            errorMsg={error}
+          >
             <p className="paragraph-link-gray">
               <Link to="/login">Back to login</Link>
             </p>
-          </div>
-        )}
+          </AuthForm>
+        </Fragment>
       </AuthWrapper>
     </div>
   );
--- src/views/SetNewPassword/form-model.js
+++ src/views/SetNewPassword/form-model.js
@@ -1,9 +1,11 @@
 export const initialValues = {
+  code: '',
   newPassword: '',
   newPasswordConfirmation: ''
 };
 
 export const formFields = [
+  { label: 'Confirmation code', name: 'code', type: 'text' },
   {
     label: 'New Password',
     name: 'newPassword',
