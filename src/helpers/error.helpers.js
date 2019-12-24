61d3fdb61fff14d02efeeaf77928ad251bd437a7
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-12-24 11:05:43 +0100

    Edit error handling

--- src/helpers/error.helpers.js
+++ src/helpers/error.helpers.js
@@ -1,12 +1,17 @@
-export function handleError (error) {
+export function handleError(error) {
   if (error.response) {
     const errorData = error.response.data;
+    const statusCode = error.response.status;
 
     let errorMessage;
     if (errorData) {
-      errorMessage = errorData.message
-        ? `${errorData.status} ${errorData.message}`
-        : `${errorData.status} ${errorData.error}`;
+      if (errorData.message) {
+        errorMessage = `${statusCode} ${errorData.message}`;
+      } else if (errorData.error_description) {
+        errorMessage = `${statusCode} ${errorData.error_description}`;
+      } else {
+        errorMessage = `${statusCode} ${errorData.error}`;
+      }
 
       return Promise.reject(errorMessage);
     }
