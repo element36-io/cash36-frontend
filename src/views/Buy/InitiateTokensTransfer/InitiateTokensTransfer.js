a2764608bc9b189d1be5196755148e01facde6de
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-01-29 16:40:44 +0100

    Add UI for transfer

---
+++ src/views/Buy/InitiateTokensTransfer/InitiateTokensTransfer.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { CircularProgress } from '@material-ui/core';
+
+import DefaultButton from '../../../components/Buttons/DefaultButton';
+import SecondaryButton from '../../../components/Buttons/SecondaryButton';
+
+import './InitiateTokensTransfer.scss';
+
+const InitiateTokensTransfer = () => {
+  const [checkingSender, setCheckingSender] = useState(true);
+  const [checkingReceiver, setCheckingReceiver] = useState(true);
+
+  const verifyKyc = () => {
+    setCheckingSender(true);
+    setCheckingReceiver(true);
+
+    // Blockchain code for checking
+
+    setCheckingSender(false);
+    setCheckingReceiver(false);
+  };
+
+  useEffect(() => {
+    verifyKyc('sender');
+    verifyKyc('receiver');
+  }, []);
+
+  return (
+    <div>
+      <div className="initiate-tokens-transfer">
+        <h2>Initiate Tokens Transfer</h2>
+        <div className="initiate-tokens-transfer__kyc-verifications">
+          <div className="initiate-tokens-transfer__kyc-verification">
+            <h4>{checkingSender ? 'Verifying KYC for Sender...' : 'Sender'}</h4>
+            {checkingSender && <CircularProgress color="primary" size={15} />}
+          </div>
+          <div className="initiate-tokens-transfer__kyc-verification">
+            <h4>
+              {checkingSender ? 'Verifying KYC for Receiver...' : 'Receiver'}
+            </h4>
+            {checkingReceiver && <CircularProgress color="primary" size={15} />}
+          </div>
+        </div>
+
+        {(!checkingSender || !checkingReceiver) && (
+          <div className="initiate-tokens-transfer__buttons">
+            <DefaultButton>Execute Order 66</DefaultButton>
+            <SecondaryButton>Cancel</SecondaryButton>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InitiateTokensTransfer;
