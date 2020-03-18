046bca3a7aad0855ea74febaa5bec5008b86dfbd
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-18 15:08:04 +0100

    change error handling

--- src/helpers/error.helpers.js
+++ src/helpers/error.helpers.js
@@ -1,11 +1,16 @@
 export function handleError(error) {
   if (error.response) {
-    console.log(error.response);
-
     const errorData = error.response.data;
     const statusCode = error.response.status;
 
     let errorMessage;
+
+    if (Array.isArray(error.response.data)) {
+      errorMessage = error.response.data.join(';');
+
+      return Promise.reject(errorMessage);
+    }
+
     if (errorData) {
       if (errorData.message) {
         errorMessage = `${errorData.message} (${statusCode})`;
