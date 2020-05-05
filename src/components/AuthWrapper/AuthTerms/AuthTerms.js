1b9d5d15c52026600dbf1591ca196f5e9a886b41
walter.strametz@gmail.com <walter.strametz@gmail.com>
2020-05-05 16:30:12 +0200

    noopener; update cypress

--- src/components/AuthWrapper/AuthTerms/AuthTerms.js
+++ src/components/AuthWrapper/AuthTerms/AuthTerms.js
@@ -4,11 +4,11 @@ import './AuthTerms.scss';
 const LoginTerms = () => (
   <div className="login__terms">
     By signing in, you agree to{' '}
-    <span>element36 <a 
+    <span>element36 <a  
     href="https://docs.google.com/document/d/e/2PACX-1vQrdc8Y2IXX_wijlgpl5-hW2n18coP-hHup8LwUhQEiD787wx6tiOzvK4IEBawNYC0bTuKueO0M9Lqj/pub"
-    target="_blank" >Terms and Conditions</a> & <a 
+    target="_blank"  rel="noopener noreferrer" >Terms and Conditions</a> & <a 
     href="https://docs.google.com/document/d/e/2PACX-1vSOcC_zA-j1mJMyTsXDHktybyyDEUsU3XE9-kINF2xNCRhhlQ8ul3UYiKpVxRcW4m0k-Bxt7aglHuCV/pub" 
-    target="_blank">Privacy Policy</a></span>
+    target="_blank"  rel="noopener noreferrer">Privacy Policy</a></span>
   </div>
 );
 
