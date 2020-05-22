612e725f1eec4798eff4c72edea566a2fcfb155e
walter.strametz@gmail.com <walter.strametz@gmail.com>
2020-05-22 13:45:44 +0200

    undefined (400) error when user not known

--- src/helpers/error.helpers.js
+++ src/helpers/error.helpers.js
@@ -2,22 +2,29 @@ export function handleError(error) {
   if (error.response) {
     const errorData = error.response.data;
     const statusCode = error.response.status;
+    const statusText = error.response.statusText;
+    
 
     let errorMessage;
 
     if (Array.isArray(error.response.data)) {
-      errorMessage = `${error.response.data.join('; ')} (${statusCode})`;
-
+      errorMessage = `${error.response.data.join('; ')} (${statusCode}: ${statusText})`;
       return Promise.reject(errorMessage);
     }
 
     if (errorData) {
       if (errorData.message) {
-        errorMessage = `${errorData.message} (${statusCode})`;
+        errorMessage = `${errorData.message} (${statusCode}: ${statusText})`;
       } else if (errorData.error_description) {
-        errorMessage = `${errorData.error_description} (${statusCode})`;
+        errorMessage = `${errorData.error_description} (${statusCode}: ${statusText}))`;
+      } else if (errorData.error)
+        errorMessage = `${errorData.error} (${statusCode}: ${statusText}))`;
       } else {
-        errorMessage = `${errorData.error} (${statusCode})`;
+        if (error.respnse.status=404) {
+          errorMessage = `Ressource (or user)  not found (${statusCode}: ${statusText}))`;
+        } else {
+          errorMessage = `Error from server: (${statusCode}: ${statusText}))`;
+        }
       }
 
       return Promise.reject(errorMessage);
